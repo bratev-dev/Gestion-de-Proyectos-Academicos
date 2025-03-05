4cae68b772dbe071db001fbdfc65f9ff7b0d2c5a
JuanDv <juanperdomobp@unicauca.edu.co>
2025-03-05 08:26:37 -0500

    Renombrado de paquete entidades

---
+++ GestionProyectosAcademicos/src/main/java/com/mycompany/projects/domain/entities/GestionProyectosAcademicos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projects.domain.entities;
+
+/**
+ *
+ * @author bryan
+ */
+public class GestionProyectosAcademicos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
