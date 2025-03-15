f4d93c5b205d8b86a3674c3258d3f09f2ad10d76
Jhonatan Palacios Gomez <jpalaciosg19@gmail.com>
2025-03-15 13:35:44 -0500

    1. Creacion de paquete "Observer", para implementacion del mismo patron. Creacion de Interfaz Observer 2 .Creacion de paquete "State". Creacion de interfaz "ProjectState", clases: Accepted, Received y Rejected. 3. Refactorizacion de atributos de la clase "Student". 4. Creacion de clase "Request" en el paquete "entities". 5. Implementacion de atributos de la clase "Project"

--- GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/entities/Project.java
+++ GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/entities/Project.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.gestionproyectosacademicos.entities;
 
+import com.mycompany.gestionproyectosacademicos.observer.Observer;
+import com.mycompany.gestionproyectosacademicos.state.ProjectState;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author bryan
  */
 public class Project {
-    
+    private int id;
+    private String name;
+    private String description;
+    private String state;
+    private int qualification;
+    private Request request;
+    private Company company;
+    private Student[] students;
+    private List<Observer> observadores = new ArrayList<>();
 }
