ba3d57717efa7a67664bb6195afd6958fa15c1fa
Brayan Gomes <gbrayansteven2@gmail.com>
2025-03-16 14:15:59 -0500

    Guardando cambios pendientes en Main.java antes de la fusión

--- GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/main/Main.java
+++ GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/main/Main.java
@@ -4,10 +4,9 @@
  */
 package com.mycompany.gestionproyectosacademicos.main;
 
-<<<<<<< HEAD
 import com.mycompany.gestionproyectosacademicos.presentation.GUICoordinator;
 import com.mycompany.gestionproyectosacademicos.presentation.GUILogin;
-=======
+
 import com.mycompany.gestionproyectosacademicos.access.Factory;
 import com.mycompany.gestionproyectosacademicos.access.IUserRepository;
 import com.mycompany.gestionproyectosacademicos.access.UserArrayRepository;
@@ -15,7 +14,6 @@ import com.mycompany.gestionproyectosacademicos.presentacion.GUICoordinator;
 import com.mycompany.gestionproyectosacademicos.presentacion.GUILogin;
 import com.mycompany.gestionproyectosacademicos.services.AuthService;
 import com.mycompany.gestionproyectosacademicos.services.UserServices;
->>>>>>> 8b1a1ffa02edac09e15fd5975f71496af03b5a5f
 import javax.swing.JFrame;
 
 /**
@@ -33,21 +31,16 @@ public class Main {
         instance.setVisible(true);
         */
       
-        /* RAMA JONATHAN
         GUICoordinator instance = new GUICoordinator();
         instance.setExtendedState(JFrame.MAXIMIZED_BOTH);
         instance.setVisible(true);
-        */
+        
       
-        /* RAMA BRATEV
-        GUILogin login = new GUILogin();
-        login.setVisible(false);
-
-        IUserRepository userRepo = new UserArrayRepository(); // Usando datos en memoria
+        /*IUserRepository userRepo = new UserArrayRepository(); // Usando datos en memoria
         AuthService authService = new AuthService(userRepo);
 
         GUILogin loginGUI = new GUILogin(authService);
-        loginGUI.setVisible(true);
-        */
+        loginGUI.setVisible(true);*/
+       
     }
 }
