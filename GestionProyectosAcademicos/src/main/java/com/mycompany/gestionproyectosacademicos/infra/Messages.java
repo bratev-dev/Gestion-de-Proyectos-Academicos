4e5391eeeebf7400fd288c245b8690257a026ede
JuanDv <juanperdomobp@unicauca.edu.co>
2025-03-16 14:47:40 -0500

    Modificación GUILogin y Messages

--- GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/infra/Messages.java
+++ GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/infra/Messages.java
@@ -4,8 +4,16 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author bryan
+ * @author Libardo Pantoja, Julio A. Hurtado
  */
 public class Messages {
-    
+
+    public static void showMessageDialog(String message, String title) {
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static int showConfirmDialog(String message, String title) {
+        return JOptionPane.showConfirmDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
