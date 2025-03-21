059396b09bfee6b9211d5f5ffa6e2ac2f4f57c5b
Brayan Gomes <gbrayansteven2@gmail.com>
2025-03-20 23:19:46 -0500

    Eliminación de clases Request, SaveDateCompany, StudentServices con refactorizado (por eso se afectan otras clases que tenían algunos imports)

--- GestionProyectosAcademicos/src/main/java/com/mycompany/gestionproyectosacademicos/services/StudentServices.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.gestionproyectosacademicos.services;
-
-/**
- *
- * @author Jhonatan
- */
-public class StudentServices {
-    //POR IMPLEMENTAR Y DECIDIR SI EL PAQUETE "SERVICES" SE MANTIENE O SE ELIMINA
-}
